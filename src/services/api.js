e8fc75c31f5b30cc6e415c0c79dc2decf24a77c5
yogesh kumawat <yogi2006kumar@gmail.com>
2026-04-18 17:15:54 +0530

    update frontend/backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://jobportal-backend-ew1j.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
